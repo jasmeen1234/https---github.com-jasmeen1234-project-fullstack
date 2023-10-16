05398d81708e8b1bce0ac1f298bcffee410ae6f0
jasmeen1234 <jasmeenbano12@gmail.com>
2023-10-16 11:15:37 +0530

    first commit

---
+++ backend/userSchema.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+let userSchema = new Schema({
+  id: String,
+  email: String,
+  password: String,
+});
+module.exports = mongoose.model("user", userSchema);
